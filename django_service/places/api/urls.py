1b1e272900211ab7924204520e3b0c34945dbeec
Vitaly L <111348000+vetrof@users.noreply.github.com>
2025-01-20 00:01:27 +0500

    add detail view

--- django_service/places/api/urls.py
+++ django_service/places/api/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import PlaceListApi, NearPlace
+from .views import PlaceListApi, NearPlace, PlaceDetail
 
 urlpatterns = [
     path('all/', PlaceListApi.as_view()),
     path('near/', NearPlace.as_view()),
+    path('detail/<int:id>/', PlaceDetail.as_view()),
 ]
--- django_service/places/api/views.py
+++ django_service/places/api/views.py
@@ -3,9 +3,7 @@ from rest_framework.views import APIView
 from places.models import Place
 from .serializers import PlaceSerializer
 from django.http import JsonResponse
-
 from ..services.geo_orm import near_place
-
 
 class PlaceListApi(APIView):
     def get(self, request):
@@ -19,10 +17,22 @@ class NearPlace(APIView):
         lat = request.GET.get('lat')
         lon = request.GET.get('lon')
         count = request.GET.get('count')
-        print(lat, lon, count)
 
         if lat and lon:
             answer = near_place(lat, lon, count)
             return answer
 
         return JsonResponse({'error': 'Coordinates not provided'}, status=400)
+
+
+class PlaceDetail(APIView):
+    def get(self, request, *args, **kwargs):
+        id = kwargs.get('id')
+        try:
+            place = Place.objects.get(id=id)
+        except:
+            return Response({'error': "not found this place"}, status=404)
+
+        serializer = PlaceSerializer(place, many=False)
+        return Response({'place': serializer.data})
+
